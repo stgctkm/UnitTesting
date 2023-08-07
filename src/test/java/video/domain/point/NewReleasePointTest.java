1656c13287a6cd5b6f0594eadd51fc2640294999
setoguchi <setoguchi.takuma@gmail.com>
2023-08-07 12:28:56 +0900

    fix typo

--- src/test/java/video/domain/point/NewReleasePointTest.java
+++ src/test/java/video/domain/point/NewReleasePointTest.java
@@ -6,13 +6,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class NewReleasePointTest {
     @Test
-    void 新作を1日レンタルする料金() {
+    void 新作を1日レンタルするポイント() {
         NewReleasePoint sut = new NewReleasePoint();
         assertEquals(1, sut.frequentRenterPoints(1));
     }
 
     @Test
-    void 新作を2日レンタルする料金() {
+    void 新作を2日レンタルするポイント() {
         NewReleasePoint sut = new NewReleasePoint();
         assertEquals(2, sut.frequentRenterPoints(2));
     }
