ccd5123da92d4ecbed9ea0f3cebf3877882a9b30
stgctkm <setoguchi.takuma@gmail.com>
2023-08-22 17:49:34 +0900

    PriceTypeの区分にPriceインターフェースを設定

--- src/main/java/video/domain/PriceType.java
+++ src/main/java/video/domain/PriceType.java
@@ -2,27 +2,21 @@ package video.domain;
 
 import video.domain.price.ChildrenPrice;
 import video.domain.price.NewReleasePrice;
+import video.domain.price.Price;
 import video.domain.price.RegularPrice;
 
 public enum PriceType {
-    CHILDREN,
-    REGULAR,
-    NEW_RELEASE;
+    CHILDREN(new ChildrenPrice()),
+    REGULAR(new RegularPrice()),
+    NEW_RELEASE(new NewReleasePrice());
 
-    private final RegularPrice regularPrice = new RegularPrice();
-    private final NewReleasePrice newReleasePrice = new NewReleasePrice();
-    private final ChildrenPrice childrenPrice = new ChildrenPrice();
+    Price price;
+
+    PriceType(Price price) {
+        this.price = price;
+    }
 
     int amount(int daysRented) {
-        switch (this) {
-            case REGULAR:
-                return regularPrice.amount(daysRented);
-            case NEW_RELEASE:
-                return newReleasePrice.amount(daysRented);
-            case CHILDREN:
-                return childrenPrice.amount(daysRented);
-            default:
-                throw new RuntimeException("ビデオの区分が誤っています");
-        }
+        return price.amount(daysRented);
     }
 }
