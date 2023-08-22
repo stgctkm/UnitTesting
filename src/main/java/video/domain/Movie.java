f6c1cac663c621bc323640d81d8b037d22a85ebd
stgctkm <setoguchi.takuma@gmail.com>
2023-08-22 17:42:59 +0900

    PriceTypeにメソッドを移動

--- src/main/java/video/domain/Movie.java
+++ src/main/java/video/domain/Movie.java
@@ -1,17 +1,10 @@
 package video.domain;
 
-import video.domain.price.ChildrenPrice;
-import video.domain.price.NewReleasePrice;
-import video.domain.price.RegularPrice;
-
 public class Movie {
 
     private String title;
     PriceType priceType;
 
-    private final RegularPrice regularPrice = new RegularPrice();
-    private final NewReleasePrice newReleasePrice = new NewReleasePrice();
-    private final ChildrenPrice childrenPrice = new ChildrenPrice();
 
     public Movie(String title, PriceType priceType) {
         this.title = title;
@@ -30,16 +23,8 @@ public class Movie {
         return title;
     }
 
-    public int amount(int daysRented) {
-        switch (priceType) {
-            case REGULAR:
-                return regularPrice.amount(daysRented);
-            case NEW_RELEASE:
-                return newReleasePrice.amount(daysRented);
-            case CHILDREN:
-                return childrenPrice.amount(daysRented);
-            default:
-                throw new RuntimeException("ビデオの区分が誤っています");
-        }
+    public int movieAmount(int daysRented) {
+        return priceType.amount(daysRented);
     }
+
 }
--- src/main/java/video/domain/PriceType.java
+++ src/main/java/video/domain/PriceType.java
@@ -1,7 +1,28 @@
 package video.domain;
 
+import video.domain.price.ChildrenPrice;
+import video.domain.price.NewReleasePrice;
+import video.domain.price.RegularPrice;
+
 public enum PriceType {
     CHILDREN,
     REGULAR,
-    NEW_RELEASE
+    NEW_RELEASE;
+
+    private final RegularPrice regularPrice = new RegularPrice();
+    private final NewReleasePrice newReleasePrice = new NewReleasePrice();
+    private final ChildrenPrice childrenPrice = new ChildrenPrice();
+
+    int amount(int daysRented) {
+        switch (this) {
+            case REGULAR:
+                return regularPrice.amount(daysRented);
+            case NEW_RELEASE:
+                return newReleasePrice.amount(daysRented);
+            case CHILDREN:
+                return childrenPrice.amount(daysRented);
+            default:
+                throw new RuntimeException("ビデオの区分が誤っています");
+        }
+    }
 }
