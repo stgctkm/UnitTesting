dcb14dba5f56218ccea2a62f2b19b6fd767c8ff6
stgctkm <setoguchi.takuma@gmail.com>
2023-08-22 12:41:24 +0900

    レンタルポイント算出メソッドの移動

--- src/main/java/video/domain/Customer.java
+++ src/main/java/video/domain/Customer.java
@@ -28,18 +28,11 @@ public class Customer {
         int frequentRenterPoints = 0;
         for (Rental each : rentals) {
             int thisAmount = each.amount();
-            frequentRenterPoints += frequentRenterPoints(each);
+            frequentRenterPoints += each.frequentRenterPoints();
             totalAmount += thisAmount;
         }
 
         return new RentalResult(totalAmount, frequentRenterPoints);
-    }
-
-    private int frequentRenterPoints(Rental each) {
-        // 新作を二日以上借りた場合はボーナスポイント
-        if ((each.getMovie().getPriceType() == PriceType.NEW_RELEASE) &&
-            each.getDaysRented() > 1) return 2;
-        return 1;
     }
 
 }
